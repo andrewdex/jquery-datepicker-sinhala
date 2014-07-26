92e7d7e3bdb72b707df0691af85ed60e7ccf09ed
Andrew dEX <andrewdexis@gmail.com>
2014-07-26 14:10:25 +0530

    Re-Decoded JS

--- datepicker-lk_si.js
+++ datepicker-lk_si.js
@@ -1,4 +1,37 @@
-(function(a){"function"===typeof define&&define.amd?define(["../datepicker"],a):a(jQuery.datepicker)})(function(a){a.regional["lk-si"]={closeText:"\u0d85\u0dc0\u0dc3\u0db1\u0dca",prevText:"\u0db4\u0dd9\u0dbb",nextText:"\u0d8a\u0dbd\u0d9c",currentText:"\u0d85\u0daf",monthNames:"\u0da2\u0db1\u0dc0\u0dcf\u0dbb\u0dd2 \u0db4\u0dd9\u0db6\u0dbb\u0dc0\u0dcf\u0dbb\u0dd2 \u0db8\u0dcf\u0dbb\u0dca\u0dad\u0dd4 \u0d85\u0db4\u0dca\u200d\u0dbb\u0dda\u0dbd\u0dca \u0db8\u0dd0\u0dba\u0dd2 \u0da2\u0dd4\u0db1\u0dd2 \u0da2\u0dd4\u0dbd\u0dd2 \u0d85\u0d9c\u0ddd\u0dc3\u0dca\u0dad\u0dd4 \u0dc3\u0dd0\u0db4\u0dca\u0dad\u0dd0\u0db8\u0dca\u0db6\u0dbb\u0dca \u0d94\u0d9a\u0dca\u0dad\u0ddd\u0db8\u0dca\u0db6\u0dbb\u0dca \u0db1\u0ddc\u0dc0\u0dd0\u0db8\u0dca\u0db6\u0dbb\u0dca \u0daf\u0dd9\u0dc3\u0dd0\u0db8\u0dca\u0db6\u0dbb\u0dca".split(" "),
-monthNamesShort:"\u0da2\u0db1 \u0db4\u0dd9\u0db6\u0dbb \u0db8\u0dcf\u0dbb\u0dca\u0dad\u0dd4 \u0d85\u0db4\u0dca\u200d \u0db8\u0dd0\u0dba\u0dd2 \u0da2\u0dd4\u0db1\u0dd2 \u0da2\u0dd4\u0dbd\u0dd2 \u0d85\u0d9c\u0ddd \u0dc3\u0dd0\u0db4\u0dca \u0d94\u0d9a\u0dca \u0db1\u0ddc\u0dc0\u0dd0 \u0daf\u0dd9\u0dc3\u0dd0".split(" "),dayNames:"\u0d89\u0dbb\u0dd2\u0daf\u0dcf \u0dc3\u0daf\u0dd4\u0daf\u0dcf \u0d85\u0d9c\u0dc4\u0dbb\u0dd4\u0dc0\u0dcf\u0daf\u0dcf \u0db6\u0daf\u0dcf\u0daf\u0dcf \u0db6\u0dca\u200d\u0dbb\u0dc4\u0dc3\u0dca\u0db4\u0dad\u0dd2\u0db1\u0dca\u0daf\u0dcf \u0dc3\u0dd2\u0d9a\u0dd4\u0dbb\u0dcf\u0daf\u0dcf \u0dc3\u0dd9\u0db1\u0dc3\u0dd4\u0dbb\u0dcf\u0daf\u0dcf".split(" "),
-dayNamesShort:"\u0d89\u0dbb\u0dd2 \u0dc3\u0daf\u0dd4 \u0d85\u0d9c\u0dc4 \u0db6\u0daf\u0dcf\u0daf\u0dcf \u0db6\u0dca\u200d\u0dbb\u0dc4\u0dc3\u0dca \u0dc3\u0dd2\u0d9a\u0dd4\u0dbb\u0dcf \u0dc3\u0dd9\u0db1\u0dc3".split(" "),dayNamesMin:"\u0d89\u0dbb\u0dd2 \u0dc3\u0daf\u0dd4 \u0d85\u0d9c \u0db6\u0daf\u0dcf \u0db6\u0dca\u200d\u0dbb\u0dc4 \u0dc3\u0dd2\u0d9a\u0dd4 \u0dc3\u0dd9\u0db1".split(" "),weekHeader:"\u0dc3\u0dad\u0dd2",dateFormat:"yy/mm/dd",firstDay:1,isRTL:!1,showMonthAfterYear:!1,yearSuffix:""};
-a.setDefaults(a.regional["lk-si"]);return a.regional["lk-si"]});
+/* Sinhala - Sri Lanka initialisation for the jQuery UI date picker plugin. */
+/* Written by Dilusha Gonagala - @andrewdex. */
+(function( factory ) {
+	if ( typeof define === "function" && define.amd ) {
+
+		// AMD. Register as an anonymous module.
+		define([ "../datepicker" ], factory );
+	} else {
+
+		// Browser globals
+		factory( jQuery.datepicker );
+	}
+}(function( datepicker ) {
+
+datepicker.regional['lk-si'] = {
+	closeText: 'අවසන්',
+	prevText: 'පෙර',
+	nextText: 'ඊලග',
+	currentText: 'අද',
+	monthNames: ['ජනවාරි','පෙබරවාරි','මාර්තු','අප්‍රේල්','මැයි','ජුනි',
+	'ජුලි','අගෝස්තු','සැප්තැම්බර්','ඔක්තෝම්බර්','නොවැම්බර්','දෙසැම්බර්'],
+	monthNamesShort: ['ජන', 'පෙබර', 'මාර්තු', 'අප්‍', 'මැයි', 'ජුනි',
+	'ජුලි', 'අගෝ', 'සැප්', 'ඔක්', 'නොවැ', 'දෙසැ'],
+	dayNames: ['ඉරිදා', 'සදුදා', 'අගහරුවාදා', 'බදාදා', 'බ්‍රහස්පතින්දා', 'සිකුරාදා', 'සෙනසුරාදා'],
+	dayNamesShort: ['ඉරි', 'සදු', 'අගහ', 'බදාදා', 'බ්‍රහස්', 'සිකුරා', 'සෙනස'],
+	dayNamesMin: ['ඉරි','සදු','අග','බදා','බ්‍රහ','සිකු','සෙන'],
+	weekHeader: 'සති',
+	dateFormat: 'yy/mm/dd',
+	firstDay: 1,
+	isRTL: false,
+	showMonthAfterYear: false,
+	yearSuffix: ''};
+datepicker.setDefaults(datepicker.regional['lk-si']);
+
+return datepicker.regional['lk-si'];
+
+}));
